14a31f481b206dde4ee9915fed158397df8e90a7
neuro <asuka2ndchild@gmail.com>
2020-07-06 22:52:23 +0300

    add dropdown values selector

--- src/compare/components/CompareBarDiagram.js
+++ src/compare/components/CompareBarDiagram.js
@@ -5,65 +5,93 @@ import React, {
 import MultipleBarDiagram from '../../charts/MultipleBarDiagram'
 import CompareValuesGroups from '../utils/CompareValuesGroups'
 import CompareValuesParser from '../utils/CompareValuesParser'
+import Dropdown from '../../charts/controls/Dropdown'
 
 export default function CompareBarDiagram({usersData}) {
 
   const [diagramData, setDiagramData] = useState(null)
+  const [selected, setSelected] = useState(null)
 
   useEffect(() => {
 
     const users = usersData.data
     let diagramData = []
 
+    if (users.length > 0) {
+
+      /*
+      parse data for bar diagram
+      */
+  
+      CompareValuesGroups.SCGroup.forEach(
+        entry => {
+          const dataArray = CompareValuesParser.parseNumber(
+            entry.name, entry.key, users
+          )
+          diagramData.push([entry.name, dataArray])
+        }
+      )
+  
+      CompareValuesGroups.KDGroup.forEach(
+        entry => {
+          const dataArray = CompareValuesParser.parseNumber(
+            entry.name, entry.key, users
+          )
+          diagramData.push([entry.name, dataArray])
+        }
+      )
+  
+      CompareValuesGroups.RatioGroup.forEach(
+        entry => {
+          const dataArray = CompareValuesParser.parseRatio(
+            entry.name, entry.dividend, entry.divider, users
+          )
+          diagramData.push([entry.name, dataArray])
+        }
+      )
+  
+      const parsedTime = CompareValuesParser.parseTime(users)
+      diagramData.push(["Time", parsedTime])
+  
+  
+      /*
+      set diagramData
+      */
+  
+      setDiagramData(diagramData)
+    } else {
+      setDiagramData(null)
+    }
+
     /*
-    parse data for bar diagram
+    update selected data if usersData changed
     */
 
-    CompareValuesGroups.SCGroup.forEach(
-      entry => {
-        const dataArray = CompareValuesParser.parseNumber(
-          entry.name, entry.key, users
-        )
-        diagramData.push([entry.name, dataArray])
-      }
-    )
-
-    CompareValuesGroups.KDGroup.forEach(
-      entry => {
-        const dataArray = CompareValuesParser.parseNumber(
-          entry.name, entry.key, users
-        )
-        diagramData.push([entry.name, dataArray])
-      }
-    )
-
-    CompareValuesGroups.RatioGroup.forEach(
-      entry => {
-        const dataArray = CompareValuesParser.parseRatio(
-          entry.name, entry.dividend, entry.divider, users
-        )
-        diagramData.push([entry.name, dataArray])
-      }
-    )
-
-    const parsedTime = CompareValuesParser.parseTime(users)
-    diagramData.push(["Time", parsedTime])
-
-
-    /*
-    set diagramData
-    */
-
-    setDiagramData(diagramData)
+    if (selected) {
+      const value = selected[0][0]
+      setSelected(diagramData.filter(data => data[0] === value))
+    }
 
     // stringify to catch all changes in complex data structures
     // eslint-disable-next-line
   }, [JSON.stringify(usersData.data)])
 
 
+  const onChange = value => {
+    setSelected(diagramData.filter(data => data[0] === value))
+  }
+
   return (
-    <MultipleBarDiagram
-      diagramData={diagramData}
-    />
+    diagramData
+    ? (<>
+        <Dropdown
+          values={diagramData.map(entry => ({id: entry[0], name: entry[0]}))}
+          onChange={onChange}
+        />
+        <MultipleBarDiagram
+          diagramData={selected}
+        />
+      </>)
+    : <div>No users selected</div>
   )
 }
